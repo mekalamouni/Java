f0d9d35b2e8399e0981176bb1ff8408484aad6d3
mekalamouni <134393332+mekalamouni@users.noreply.github.com>
2023-06-28 13:08:33 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+package List;
+
+public class Array{
+	public static void main(String[] args) {
+		ArrayList a =new ArrayList();
+		a.add(10);
+		a.add(20);
+		a.add(30);
+		System.out.println("size="+a.size());
+	}}
+
